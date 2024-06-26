ddc664816c25bf1e69a46dabc6a1fb7ae5e24eac
gabimancini <gabimancinir@gmail.com>
2024-06-26 15:57:33 -0300

    Add labels in form

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -6,6 +6,9 @@ const Register = () => {
                     <h1 className="title">Gen Tree</h1>
                     <form className="formulario">
                         <h2>Datos de Usuario:</h2>
+                        <label htmlFor="wallet">Wallet</label>
+                        <label htmlFor="name" style={{ marginLeft: "7.6em" }}>Full Name</label>
+                        <label htmlFor="email" style={{ marginLeft: "4.5em" }}>Email</label><br></br>
                         <input type="text" placeholder="Wallet" required />
                         <input type="text" placeholder="Full Name" required />
                         <input type="email" placeholder="Email" required /><br /><br /><br />
