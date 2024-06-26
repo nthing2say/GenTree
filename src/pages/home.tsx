55fe688f9b97f15066b404f90642a46eff5b030b
gabimancini <gabimancinir@gmail.com>
2024-06-26 15:29:01 -0300

    Init React and create home component

---
+++ src/pages/home.tsx
@@ -0,0 +1,16 @@
+const Home =()=>{
+return(
+    <main>
+    <section className="hero">
+        <div className="content">
+            <h1 className="title">Gen Tree</h1>
+            <p className="slogan">Preserva tu Historia,<br /> Asegura tu Futuro</p>
+            <button className="register-button">Regístrate Gratis</button>
+        </div>
+        <div className="content2"></div>
+    </section>
+</main>
+
+)
+}
+export default Home
