63bad6a215df6a9ee95fcd9781d8d0f8ca870184
gabimancini <gabimancinir@gmail.com>
2024-06-26 23:16:50 -0300

    Delete redirect button

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -15,7 +15,7 @@ import { useNavigate } from "react-router-dom";
                 <div className="content">
                     <h1 className="title">Gen Tree</h1>
                     <p className="slogan">Preserva tu Historia,<br /> Asegura tu Futuro</p>
-                    <button   onClick={handleRedirect}>
+            
                      <ConnectButton
                      onDisconnect={handleRedirectHome}
                      connectButton={{ 
@@ -28,8 +28,7 @@ import { useNavigate } from "react-router-dom";
                         name: "Example App",
                         url: "https://example.com",
                     }}
-                />
-                </button>
+                /> 
                 </div>
                 <div className="content2"></div>
             </section>
